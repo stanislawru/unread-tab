5d730717695c6006f5cb4de998ab5175b42cd14f
Marchenko Anton <mam@inrecolan.com>
2019-11-21 17:03:50 +0300

    Fix - store the unread tab marker when a title is auto-updating

--- visited.js
+++ visited.js
@@ -1,10 +1,13 @@
 chrome.storage.sync.get
- (
+(
   {'prefix': '• '},
   function(items)
-   {
+  {
     var prefix = items.prefix;
     var title = document.title;
-    if (title.substr(0, prefix.length) == prefix) document.title = title.substr(prefix.length, title.length);
-   }
- );
+    if (title.substr(0, prefix.length) == prefix) {
+      document.title = title.substr(prefix.length, title.length);
+      document['isReadTab'] = true;
+    }
+  }
+);
