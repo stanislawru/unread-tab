14ed12033a5c16d86c48a1945cf644c56c4eaca5
Stanislav Ogryzkov <58035665+stanislawru@users.noreply.github.com>
2019-11-22 15:05:01 +0300

    Beautified the code a little bit

--- opened.js
+++ opened.js
@@ -1,26 +1,19 @@
 chrome.storage.sync.get
-(
+ (
   {'prefix': '• '},
   function(items)
-  {
-
+   {
     var target = document.querySelector('title');
     var prefix = items.prefix;
-
-    var observer = new MutationObserver(function(mutations) {
-      mutations.forEach(function(mutation) {
-        if (!document['isReadTab_kiUZ19'] && !document.title.startsWith(prefix)) {
-          document.title = prefix + document.title;
-        }
-      });
-    });
-    var config = {
-      childList: true,
-    };
+    var observer = new MutationObserver(function(mutations)
+     {
+      mutations.forEach(function(mutation)
+       {
+        if (!document['isReadTab_kiUZ19'] && !document.title.startsWith(prefix)) document.title = prefix + document.title;
+       });
+     });
+    var config = {childList: true};
     observer.observe(target, config);
-
-    if (!document['isReadTab_kiUZ19']) {
-      document.title = prefix + document.title;
-    }
-  }
-);
+    if (!document['isReadTab_kiUZ19']) document.title = prefix + document.title;
+   }
+ );
