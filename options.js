8ac9d1befa940e908c0de3e0120bca3f5b491e9b
stanislawru <osa@inrecolan.com>
2019-12-05 10:17:03 +0300

    Minified and beautified the code a little bit

--- options.js
+++ options.js
@@ -1,37 +1,39 @@
-function translatePage() {
+function loadOptions() {chrome.storage.sync.get
+ (
+  {'prefix': '• '},
+  function(items) {document.getElementById('prefix').value = items.prefix;}
+ );}
+
+function translatePage()
+ {
   document.title = chrome.i18n.getMessage('extensionOptions');
-
   var objects = document.getElementsByTagName('*'), i;
-  for (i = 0; i < objects.length; i++)
-  {
-    if (objects[i].dataset && objects[i].dataset.message) {objects[i].innerHTML = chrome.i18n.getMessage(objects[i].dataset.message);};
-  };
-};
+  for (i = 0; i < objects.length; i++) if (objects[i].dataset && objects[i].dataset.message) {objects[i].innerHTML = chrome.i18n.getMessage(objects[i].dataset.message);}
+ }
 
-/**
- * @link https://developer.chrome.com/extensions/options
- */
-function loadOptions() {
-  chrome.storage.sync.get(
-    {'prefix': '• '},
-    function(items) {document.getElementById('prefix').value = items.prefix;}
-  );
-};
+document.addEventListener
+ (
+ 'DOMContentLoaded',
+ function ()
+   {
+    translatePage();
+    loadOptions();
+   }
+ );
 
-document.addEventListener('DOMContentLoaded', function () {
-  translatePage();
-  loadOptions();
-});
-
-function saveOptions() {
+function saveOptions()
+ {
   var prefix = document.getElementById('prefix').value;
-  chrome.storage.sync.set(
+  chrome.storage.sync.set
+   (
     {'prefix': prefix},
-    function() {
+    function()
+     {
       var status = document.getElementById('status');
       status.textContent = chrome.i18n.getMessage('optionsSaved');
       setTimeout(function() {status.textContent = '';}, 750);
-    }
-  );
-};
+     }
+   );
+ }
+
 document.getElementById('save').addEventListener('click', saveOptions);
