27fc3f41286980264dc8df88a958a62c41a11983
Marchenko Anton <mam@inrecolan.com>
2019-12-09 10:52:43 +0300

    Refactoring: move client execution scripts to background script

--- background.js
+++ background.js
@@ -1,19 +1,40 @@
-function handleActivated(activeInfo) {
-    function onExecuted(result) {
-    }
+const addTitleMarker = (prefix) => "document.title = '" + prefix + "' + document.title;";
+const removeTitleMarker = (prefix) => "document.title = document.title.substr(" + prefix.length + ", document.title.length);";
+
+chrome.storage.sync.get
+(
+  {'prefix': '• '},
+  (items) => {
+   const prefix = items.prefix;
+
+   chrome.tabs.onActivated.addListener((activeInfo) => {
     chrome.tabs.get(activeInfo.tabId, function (tab) {
-        if (tab.url) if (tab.url.startsWith('http')) chrome.tabs.executeScript(activeInfo.tabId, {file: '/visited.js'}, onExecuted);
+     if (
+       tab.url
+       && tab.url.startsWith('http')
+       && (tab.title || '').startsWith(prefix)
+     ) {
+      chrome.tabs.executeScript(activeInfo.tabId, {
+       code: removeTitleMarker(prefix)
+      })
+     }
     })
-}
+   });
 
-chrome.tabs.onActivated.addListener(handleActivated);
-
-function handleUpdated(tabId, changeInfo, tabInfo) {
-    if (changeInfo.status === 'complete') {
-        function onExecuted(result) {
-        }
-        if (tabInfo.url) if ((!tabInfo.active) && tabInfo.url.startsWith('http')) chrome.tabs.executeScript(tabId, {file: '/opened.js'}, onExecuted);
+   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+    if (
+      changeInfo.status === 'complete'
+      && !tabInfo.active
+      && tabInfo.url
+      && tabInfo.url.startsWith('http')
+      && !(tabInfo.title || '').startsWith(prefix)
+    ) {
+     chrome.tabs.executeScript(tabId, {
+      code: addTitleMarker(prefix)
+     })
     }
-}
+   });
+  }
+);
 
-chrome.tabs.onUpdated.addListener(handleUpdated);
+
