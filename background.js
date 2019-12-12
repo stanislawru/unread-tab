85ba3d7af694f9f3a2612dde9e01faa74adca6f8
stanislawru <osa@inrecolan.com>
2019-12-12 13:48:11 +0300

    Reformatted the code with PhpStorm's defaults

--- background.js
+++ background.js
@@ -1,41 +1,41 @@
 const setPageTitleClientScript = (title) => "document.title = '" + title + "';";
 const setTabTitle = (tabId, newTitle) => chrome.tabs.executeScript(tabId, {
-  code: setPageTitleClientScript(newTitle)
+    code: setPageTitleClientScript(newTitle)
 });
 
 chrome.storage.sync.get
 (
-  {'prefix': '• '},
-  (items) => {
-   const prefix = items.prefix;
+    {'prefix': '• '},
+    (items) => {
+        const prefix = items.prefix;
 
-   chrome.tabs.onActivated.addListener((activeInfo) => {
-    chrome.tabs.get(activeInfo.tabId, function (tab) {
-      const tabTitle = tab.title || '';
-     if (
-       tab.url
-       && tab.url.startsWith('http')
-       && tabTitle.trim().startsWith(prefix.trim())
-     ) {
-       const deleteCount = prefix > tabTitle.length ? tabTitle.length : prefix.length;
-       const beginSliceIndex = deleteCount - 1 > 0 ? deleteCount - 1 : 0;
-       const newTitleWithoutPrefix = tabTitle.slice(beginSliceIndex);
-       setTabTitle(activeInfo.tabId, newTitleWithoutPrefix);
-     }
-    })
-   });
+        chrome.tabs.onActivated.addListener((activeInfo) => {
+            chrome.tabs.get(activeInfo.tabId, function (tab) {
+                const tabTitle = tab.title || '';
+                if (
+                    tab.url
+                    && tab.url.startsWith('http')
+                    && tabTitle.trim().startsWith(prefix.trim())
+                ) {
+                    const deleteCount = prefix > tabTitle.length ? tabTitle.length : prefix.length;
+                    const beginSliceIndex = deleteCount - 1 > 0 ? deleteCount - 1 : 0;
+                    const newTitleWithoutPrefix = tabTitle.slice(beginSliceIndex);
+                    setTabTitle(activeInfo.tabId, newTitleWithoutPrefix);
+                }
+            })
+        });
 
-   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-    if (
-      changeInfo.status === 'complete'
-      && !tabInfo.active
-      && tabInfo.url
-      && tabInfo.url.startsWith('http')
-      && !(tabInfo.title || '').startsWith(prefix)
-    ) {
-      const newTitle = prefix + tabInfo.title;
-      setTabTitle(tabId, newTitle);
+        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+            if (
+                changeInfo.status === 'complete'
+                && !tabInfo.active
+                && tabInfo.url
+                && tabInfo.url.startsWith('http')
+                && !(tabInfo.title || '').startsWith(prefix)
+            ) {
+                const newTitle = prefix + tabInfo.title;
+                setTabTitle(tabId, newTitle);
+            }
+        });
     }
-   });
-  }
 );
