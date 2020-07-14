f524f5b0a536f29ae473aefb8a047a6e56cb2200
Stanislav Ogryzkov <58035665+stanislawru@users.noreply.github.com>
2020-07-14 07:01:39 +0300

    Fixed the mark/prefix for reloaded tabs
    Issue #9

--- background.js
+++ background.js
@@ -12,6 +12,7 @@ chrome.storage.sync.get
         chrome.tabs.onActivated.addListener((activeInfo) => {
             chrome.tabs.get(activeInfo.tabId, function (tab) {
                 const tabTitle = tab.title || '';
+                const varName = 'chomeTab' + activeInfo.tabId + 'status';
                 if (
                     tab.url
                     && tab.url.startsWith('http')
@@ -21,21 +22,31 @@ chrome.storage.sync.get
                     const beginSliceIndex = deleteCount - 1 > 0 ? deleteCount - 1 : 0;
                     const newTitleWithoutPrefix = tabTitle.slice(beginSliceIndex);
                     setTabTitle(activeInfo.tabId, newTitleWithoutPrefix);
+                    var object = {};
+                    object[varName] = 'Activated';
+                    chrome.storage.local.set(object);
                 }
             })
         });
 
         chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-            if (
-                changeInfo.status === 'complete'
-                && !tabInfo.active
-                && tabInfo.url
-                && tabInfo.url.startsWith('http')
-                && !(tabInfo.title || '').startsWith(prefix)
-            ) {
-                const newTitle = prefix + tabInfo.title;
-                setTabTitle(tabId, newTitle);
-            }
+            const varName = 'chomeTab' + tabId + 'status';
+            chrome.storage.local.get([varName], function(value) {
+                if (value[varName] != 'Activated') {
+                    if (
+                        !tabInfo.active
+                        && tabInfo.url
+                        && tabInfo.url.startsWith('http')
+                        && !(tabInfo.title || '').startsWith(prefix)
+                    ) {
+                        const newTitle = prefix + tabInfo.title;
+                        setTabTitle(tabId, newTitle);
+                    };
+                    var object = {};
+                    object[varName] = 'Updated';
+                    chrome.storage.local.set(object);
+		};
+            });
         });
     }
 );
