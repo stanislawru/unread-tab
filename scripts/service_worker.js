c7f633583c870e63bdb53639ee6fb56071925f4f
Stanislav Ogryzkov <58035665+stanislawru@users.noreply.github.com>
2024-04-08 14:24:11 +0300

    Catch exceptions, tab formatting, misprint in chRomeTab and onRemoved listener
    catch() added for exceptions in setTabTitle, tab formatting fixed (spaces were used in some places), a misprint fixed in stored variables' names (“chomeTab…” instead of “chromeTab…”), onRemoved listener added to removed no longer needed variables from the local storage.

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -4,57 +4,53 @@ function setTabTitle(tabId, title) {chrome.scripting.executeScript({
 	target: {tabId: tabId, allFrames: true},
 	func: setDocumentTitle,
 	args: [title]
-});}
+}).catch();}
 
 chrome.storage.sync.get
 (
 
-    {'prefix': '• '},
+	{'prefix': '• '},
 
-    (items) => {
+	(items) => {
 
-        const prefix = items.prefix;
+		const prefix = items.prefix;
 
-        chrome.tabs.onActivated.addListener((activeInfo) => {
-            chrome.tabs.get(activeInfo.tabId, function (tab) {
-                const tabTitle = tab.title || '';
-                const varName = 'chomeTab' + activeInfo.tabId + 'status';
-                if (
-                    tab.url
-                    && tab.url.startsWith('http')
-                    && tabTitle.trim().startsWith(prefix.trim())
-                ) {
-                    const deleteCount = prefix > tabTitle.length ? tabTitle.length : prefix.length;
-                    const beginSliceIndex = deleteCount - 1 > 0 ? deleteCount - 1 : 0;
-                    const newTitleWithoutPrefix = tabTitle.slice(beginSliceIndex);
-                    setTabTitle(activeInfo.tabId, newTitleWithoutPrefix);
-                };
-                var object = {};
-                object[varName] = 'Activated';
-                chrome.storage.local.set(object);
-            })
-        });
+		chrome.tabs.onActivated.addListener((activeInfo) => {
+			chrome.tabs.get(activeInfo.tabId, function (tab) {
+				const tabTitle = tab.title || '';
+				const varName = 'chromeTab' + activeInfo.tabId + 'status';
+				if (tab.url && tab.url.startsWith('http') && tabTitle.trim().startsWith(prefix.trim())) {
+					const deleteCount = prefix > tabTitle.length ? tabTitle.length : prefix.length;
+					const beginSliceIndex = deleteCount - 1 > 0 ? deleteCount - 1 : 0;
+					const newTitleWithoutPrefix = tabTitle.slice(beginSliceIndex);
+					setTabTitle(activeInfo.tabId, newTitleWithoutPrefix);
+				};
+				var object = {};
+				object[varName] = 'Activated';
+				chrome.storage.local.set(object);
+			})
+		});
 
-        chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
-            const varName = 'chomeTab' + tabId + 'status';
-            chrome.storage.local.get([varName], function(value) {
-                if (value[varName] != 'Activated') {
-                    if (
-                        !tabInfo.active
-                        && tabInfo.url
-                        && tabInfo.url.startsWith('http')
-                        && !(tabInfo.title || '').startsWith(prefix)
-                    ) {
-                        const newTitle = prefix + tabInfo.title;
-                        setTabTitle(tabId, newTitle);
-                    };
-                    var object = {};
-                    object[varName] = 'Updated';
-                    chrome.storage.local.set(object);
-		};
-            });
-        });
+		chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+			const varName = 'chromeTab' + tabId + 'status';
+			localStorage.removeItem(varName);
+            	});
 
-    }
+		chrome.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+			const varName = 'chromeTab' + tabId + 'status';
+			chrome.storage.local.get([varName], function(value) {
+				if (value[varName] != 'Activated') {
+					if (!tabInfo.active && tabInfo.url && tabInfo.url.startsWith('http') && !(tabInfo.title || '').startsWith(prefix)) {
+						const newTitle = prefix + tabInfo.title;
+						setTabTitle(tabId, newTitle);
+					};
+					var object = {};
+					object[varName] = 'Updated';
+					chrome.storage.local.set(object);
+				};
+			});
+        	});
+
+	}
 
 );
